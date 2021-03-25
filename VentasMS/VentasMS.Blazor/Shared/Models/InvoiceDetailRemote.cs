a6503b35a307fa8e7d13288dab70c94322a93088
Andres Felipe Diaz <andresdiaz3011@hotmail.com>
2021-03-25 13:44:00 -0500

    Se agrega blazor al proyecto

---
+++ VentasMS/VentasMS.Blazor/Shared/Models/InvoiceDetailRemote.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VentasMS.Blazor.Shared.Models
+{
+    public class InvoiceDetailRemote
+    {
+        public string ArticuloGuid { get; set; }
+
+        public int Quantity { get; set; }
+
+        public int Price { get; set; }
+
+        public string NombreArticulo { get; set; }
+
+        public string Unidad { get; set; }
+
+        public string Marca { get; set; }
+    }
+}
---
+++ VentasMS/VentasMS.Blazor/Shared/Models/InvoiceRemote.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VentasMS.Blazor.Shared.Models
+{
+    public class InvoiceRemote
+    {
+        public int NumberInvoice { get; set; }
+
+        public DateTime DateInvoice { get; set; }
+
+        public string Description { get; set; }
+
+        public List<InvoiceDetailRemote> Details { get; set; }
+    }
+}
